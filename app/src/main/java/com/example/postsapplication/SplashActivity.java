f4e9ccb44722612aa24e33d5cf585095bff13766
Yasmine221 <yasmeenessam22@gmail.com>
2022-10-12 19:11:44 +0200

    adding navigation component

---
+++ app/src/main/java/com/example/postsapplication/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.postsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.splash_background);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        },3000);
+
+
+
+    }
+}
